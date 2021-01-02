9c62ae65a89b3e7fe8c371cd38a3e8131c838273
陈柳鹏 <vclp@qq.vip.com>
2021-01-02 20:16:41 +0800

    feat: 全局提示和异常处理

--- config/config.default.js
+++ config/config.default.js
@@ -6,42 +6,42 @@
  * @param {Egg.EggAppInfo} appInfo app info
  */
 module.exports = appInfo => {
-  /**
-   * built-in config
-   * @type {Egg.EggAppConfig}
-   **/
-  const config = exports = {};
+	/**
+	 * built-in config
+	 * @type {Egg.EggAppConfig}
+	 **/
+	const config = exports = {};
 
-  // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1609587093342_4039';
+	// use for cookie sign key, should change to your own and keep security
+	config.keys = appInfo.name + '_1609587093342_4039';
 
-  // add your middleware config here
-  config.middleware = [];
+	// add your middleware config here
+	config.middleware = ['errorHandler'];
 
-  // add your user config here
-  const userConfig = {
-    // myAppName: 'egg',
-  };
-	
+	// add your user config here
+	const userConfig = {
+		// myAppName: 'egg',
+	};
+
 	config.security = {
-	    // 关闭 csrf
-	    csrf: {
-	      headerName: 'x-csrf-token',
-	      ignore: ctx => {
-	        return ctx.request.url.startsWith('/api')
-	      },
-	    },
-	    // 跨域白名单
-	    // domainWhiteList: ['http://localhost:3000'],
-	  };
-	  // 允许跨域的方法
-	  config.cors = {
-	    origin: '*',
-	    allowMethods: 'GET, PUT, POST, DELETE, PATCH'
-	  };
-	
-  return {
-    ...config,
-    ...userConfig,
-  };
+		// 关闭 csrf
+		csrf: {
+			headerName: 'x-csrf-token',
+			ignore: ctx => {
+				return ctx.request.url.startsWith('/api')
+			},
+		},
+		// 跨域白名单
+		// domainWhiteList: ['http://localhost:3000'],
+	};
+	// 允许跨域的方法
+	config.cors = {
+		origin: '*',
+		allowMethods: 'GET, PUT, POST, DELETE, PATCH'
+	};
+
+	return {
+		...config,
+		...userConfig,
+	};
 };
